1e3024d31021c63cde8a97a5e98cacd2fa30c69c
VaishnaviKS18 <vaishnavibhat018@gmail.com>
2024-02-25 13:38:10 +0530

    Initial commit

---
+++ models/server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
